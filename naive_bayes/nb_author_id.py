c62dafaeeabd96e40b6949abf18258bd3dd53936
Johannes Birk <Johannes.Birk11@web.de>
2018-02-14 20:30:40 +0100

    Implement Naive Bayes Classifier operating on Email text input

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,11 +9,18 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
+import os
+
+##os.chdir("/home/johannes/PycharmProjects/UdacityMLIntro/BayesAnaConda")
 import sys
 from time import time
-sys.path.append("../tools/")
+
+print(os.getcwd()+" hurray")
+sys.path.append("/home/johannes/PycharmProjects/UdacityMLIntro/BayesAnaConda/tools/")
 from email_preprocess import preprocess
+
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -21,8 +28,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
 
+GaussianNB(priors=None)
+
+t1 = time()
+accuracy = clf.score(features_test, labels_test)
+print "comparing time:", round(time()-t1, 3), "s"
+
+
+print("accuracy: ")
+print(accuracy)
 
 #########################################################
 ### your code goes here ###
