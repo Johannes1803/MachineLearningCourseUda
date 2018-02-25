6dc66c815cd979c8ec043ba66d7e88cc14bbd84b
Johannes Birk <Johannes.Birk11@web.de>
2018-02-25 15:58:20 +0100

    Identify outliers in enron data set. Outliers are people with a very high salary and/ or bonuses. Distinguish between data handling errors ( exclude) and Persons of interests ( e.g. CEOs, keep since of special interest).

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,17 +10,28 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+greedy = []
+for (outer_k) in data_dict:
+        person = data_dict[outer_k]
+        if (person['salary']> 1000000 and person['salary'] != 'NaN' and  person['bonus']> 5000000 and person['salary'] != 'NaN'):
+            greedy.append(outer_k)
+for item in greedy:
+    print(item)
 
 for point in data:
-    salary = point[0]
-    bonus = point[1]
+    salary = int(point[0])
+    bonus = int(point[1])
     matplotlib.pyplot.scatter( salary, bonus )
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
-matplotlib.pyplot.show()
+matplotlib.pyplot.autoscale(False)
+#matplotlib.pyplot.xlim((0, 10000000))
+#matplotlib.pyplot.ylim((0, 10000000))
 
+matplotlib.pyplot.show()
